53223acdb63f86a73eb29d05e43c18baaa555fe9
Victor Beserra <mxd46934@gmail.com>
2023-03-29 00:15:11 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -12,6 +12,3 @@ try {
     echo "Error while trying to connect in database: ".$e->getMessage();
     die();
 }
-
-// OLD CONNECTION USED TO MYSQLI_ESCAPE_STRING
-// $conn = mysqli_connect($host, $user, $pass, $db) or die("Error while trying to connect in database!");
